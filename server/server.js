421a4ae7ab006aa1a8405771f316e72526392de8
Sofia Etchegoin <142562661+sofia-etchegoin@users.noreply.github.com>
2023-10-17 10:12:32 +1300

    Finished step 2 and added keys in the object

--- server/server.js
+++ server/server.js
@@ -25,10 +25,7 @@ server.use(express.static(__dirname + '/public'))
 
 server.get('/', (req, res) => {
   const viewData = {
-    title: 'Gallery',
-    img: 'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2043&q=80',
-    artist: 'Manja Vitolic',
-    license: 'Unsplash License',
+    artists: art.default,
   }
 
   res.render('home', viewData)
